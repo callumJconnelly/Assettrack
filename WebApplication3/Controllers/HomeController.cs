10006a6cac3773c0a52ef13383dbd1c790999d95
Callum Connelly <Callum.Connelly@parcelhub.co.uk>
2020-10-08 21:13:48 +0100

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication3.Data;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(AppDbContext ctx)
+        {
+            _ctx = ctx;
+
+        }
+
+           private AppDbContext _ctx;
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            _ctx.Posts.Add(post);
+            await _ctx.SaveChangesAsync();
+
+            return RedirectToAction("index");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication3/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string SecondName { get; set; } = "";
+        public string Type { get; set; } = "";
+        public string AssetNumber { get; set; } = "";
+        public string Screen { get; set; } = "";
+        public string AssetNumbers { get; set; } = "";
+        public bool HeadsetIncluded { get; set; }
+        public bool KeyboardMouse { get; set; }
+        public bool DockingStation { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
+
